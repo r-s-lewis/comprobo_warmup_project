16bfc2ab04f49487e290ae791f4493c31626196a
Reuben Lewis <rlewis@olin.edu>
2023-09-08 14:39:01 -0400

    first commit

---
+++ build/warmup_project/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/reuben/ros2_ws/src/mini_projects/warmup_project/install/warmup_project'
---
+++ build/warmup_project/share/warmup_project/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/reuben/ros2_ws/src/mini_projects/warmup_project/build/warmup_project"
